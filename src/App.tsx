7249ce8d94a5ba4a450db10372052275e6a8cfc2
Ankit Kokane <ankitkokane90@gmail.com>
2024-01-19 23:29:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=''>
+      hello guys
+    </div>
+    </>
+  )
+}
+
+export default App
